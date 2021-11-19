da1ccfc7839ae951e7b8b6c0784ae6e658dda3cf
Becki Floyd <beckifloyd@gmail.com>
2021-11-19 16:08:04 +0000

    Image sizing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import SingleCard from "./components/SingleCard";
 
 const cardImages = [
   
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { "src": "/reactmemory/img/helmet-1.png", matched: false },
+  { "src": "/reactmemory/img/potion-1.png", matched: false },
+  { "src": "/reactmemory/img/ring-1.png", matched: false },
+  { "src": "/reactmemory/img/scroll-1.png", matched: false },
+  { "src": "/reactmemory/img/shield-1.png", matched: false },
+  { "src": "/reactmemory/img/sword-1.png", matched: false },
 ]
 
 function App() {
